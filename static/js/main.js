0a79a76bd655728bfdcaf525a3dba7853e3d4a49
sokhenguchiha <sokhengchen@gmail.com>
2018-08-26 12:08:34 +0700

    add the folder of html and css

---
+++ static/js/main.js
@@ -0,0 +1,7 @@
+var result = document.querySelector(".result");
+var btn = document.getElementById("form");
+btn.addEventListener("submit",function(){
+         result.style.display="block";
+         console.log("U are awesome");
+});
+
